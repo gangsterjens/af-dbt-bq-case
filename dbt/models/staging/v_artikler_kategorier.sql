a723b362b7d45a52359d9245d4ef31a6957ec0c3
Øystein Skjellfjord <90387893+gangsterjens@users.noreply.github.com>
2025-10-27 12:27:02 +0100

    added dbt-image

---
+++ dbt/models/staging/v_artikler_kategorier.sql
@@ -0,0 +1,8 @@
+{{ config(materialized='view') }}
+
+SELECT
+  lantern_id, 
+  category,
+  category_name,
+  cast(is_default as boolean) as is_default
+FROM {{ source('case_data_raw', 'artikler_kategorier') }}
